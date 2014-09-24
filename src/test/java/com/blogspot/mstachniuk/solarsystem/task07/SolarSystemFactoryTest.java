6aa9cd75d9bfa5ecd88527e9b1eb1c3ea4501dfa
Marcin Stachniuk <mstachniuk@gmail.com>
2014-09-24 11:23:27 +0200

    fix bug in assertions with BigDecimal

--- src/test/java/com/blogspot/mstachniuk/solarsystem/task07/SolarSystemFactoryTest.java
+++ src/test/java/com/blogspot/mstachniuk/solarsystem/task07/SolarSystemFactoryTest.java
@@ -31,7 +31,7 @@ public class SolarSystemFactoryTest {
         Planet mercury = innerPlanets.get(0);
         assertEquals("Mercury", mercury.getName());
         assertEquals(RotationDirection.LEFT, mercury.getRotationDirection());
-        assertEquals(new BigDecimal("4879400"), mercury.getDiameter().getMeter());
+        assertEquals(0, new BigDecimal("4879400").compareTo(mercury.getDiameter().getMeter()));
         assertEquals(0, new BigDecimal("87.96935").compareTo(mercury.getSiderealYear().inEarthDays()));
         assertEquals(3.701, mercury.getAcceleration(), 0.01);
         Set<Gas> mercuryGases = Stream.of(Gas.OXYGEN, Gas.SODIUM, Gas.HYDROGEN).collect(Collectors.toSet());
@@ -40,7 +40,7 @@ public class SolarSystemFactoryTest {
         Planet venus = innerPlanets.get(1);
         assertEquals("Venus", venus.getName());
         assertEquals(RotationDirection.RIGHT, venus.getRotationDirection());
-        assertEquals(new BigDecimal((long) 12_103_700), venus.getDiameter().getMeter());
+        assertEquals(0, new BigDecimal("12103700").compareTo(venus.getDiameter().getMeter()));
         assertEquals(0, new BigDecimal("224.70096").compareTo(venus.getSiderealYear().inEarthDays()));
         assertEquals(8.87, venus.getAcceleration(), 0.01);
         Set<Gas> venusGases = Stream.of(Gas.CARBON_DIOXIDE, Gas.NITROGEN).collect(Collectors.toSet());
@@ -49,7 +49,7 @@ public class SolarSystemFactoryTest {
         Planet earth = innerPlanets.get(2);
         assertEquals("Earth", earth.getName());
         assertEquals(RotationDirection.LEFT, earth.getRotationDirection());
-        assertEquals(new BigDecimal((long) 12_756_273), earth.getDiameter().getMeter());
+        assertEquals(0, new BigDecimal("12756273").compareTo(earth.getDiameter().getMeter()));
         assertEquals(0, new BigDecimal("365.256363004").compareTo(earth.getSiderealYear().inEarthDays()));
         assertEquals(9.806_65, earth.getAcceleration(), 0.01);
         Set<Gas> earthGases = Stream.of(Gas.NITROGEN, Gas.OXYGEN, Gas.CARBON_DIOXIDE, Gas.ARGON).collect(Collectors.toSet());
@@ -58,7 +58,7 @@ public class SolarSystemFactoryTest {
         Planet mars = innerPlanets.get(3);
         assertEquals("Mars", mars.getName());
         assertEquals(RotationDirection.LEFT, mars.getRotationDirection());
-        assertEquals(new BigDecimal((long) 6_804_900), mars.getDiameter().getMeter());
+        assertEquals(0, new BigDecimal("6804900").compareTo(mars.getDiameter().getMeter()));
         assertEquals(0, new BigDecimal("686.9601").compareTo(mars.getSiderealYear().inEarthDays()));
         assertEquals(3.69, mars.getAcceleration(), 0.01);
         Set<Gas> marsGases = Stream.of(Gas.CARBON_DIOXIDE, Gas.NITROGEN).collect(Collectors.toSet());
