fcb82f116f1c8def549dbc65d07c2f6d19af3876
Marcin Stachniuk <mstachniuk@gmail.com>
2014-09-26 11:23:42 +0200

    solution task 11

---
+++ src/test/java/com/blogspot/mstachniuk/solarsystem/task11/PlanetSetAssert.java
@@ -0,0 +1,75 @@
+package com.blogspot.mstachniuk.solarsystem.task11;
+
+import com.blogspot.mstachniuk.solarsystem.Planet;
+import org.assertj.core.api.AbstractAssert;
+import org.assertj.core.api.Assertions;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class PlanetSetAssert extends AbstractAssert<PlanetSetAssert, List<Planet>> {
+
+    private PlanetSetAssert(List<Planet> actual) {
+        super(actual, PlanetSetAssert.class);
+    }
+
+    public static PlanetSetAssert assertThat(List<Planet> actual) {
+        Assertions.assertThat(actual)
+                .isNotNull();
+        return new PlanetSetAssert(actual);
+    }
+
+    public PlanetSetAssert containsPlanetWithName(String expectedPlanetName) {
+        for (Planet planet : actual) {
+            if(planet.getName().equals(expectedPlanetName)) {
+                return this;
+            }
+        }
+        throw new AssertionError("Actual Set doesn't contains Planet with name: " + expectedPlanetName);
+    }
+
+    public PlanetSetAssert containsPlanetWithName2(String expectedPlanetName) {
+        Planet expectedPlanet = new Planet(expectedPlanetName, null, null, null);
+
+        Assertions.assertThat(actual)
+                .usingElementComparator(new PlanetNameComparator())
+                .contains(expectedPlanet);
+        return this;
+    }
+
+    public PlanetAssert containsPlanetWithName3(String expectedPlanetName) {
+        Planet expectedPlanet = new Planet(expectedPlanetName, null, null, null);
+
+        PlanetNameComparator comparator = new PlanetNameComparator();
+        Assertions.assertThat(actual)
+                .usingElementComparator(comparator)
+                .contains(expectedPlanet);
+
+        for (Planet planet : actual) {
+            if(comparator.compare(planet, expectedPlanet) == 0) {
+                return PlanetAssert.assertThat(planet);
+            }
+        }
+        return null;
+    }
+
+    public PlanetSetAssert containsOnlyPlanets(String... planets) {
+        List<Planet> expectedPlanets = new ArrayList<>();
+        for (String planet : planets) {
+            Planet p = new Planet(planet, null, null, null);
+            expectedPlanets.add(p);
+        }
+        Assertions.assertThat(actual)
+                .usingElementComparator(new PlanetNameComparator())
+                .containsOnly(expectedPlanets.toArray(new Planet[expectedPlanets.size()]));
+        return this;
+    }
+
+    private class PlanetNameComparator implements Comparator<Planet> {
+        @Override
+        public int compare(Planet p1, Planet p2) {
+            return p1.getName().compareTo(p2.getName());
+        }
+    }
+}
--- src/test/java/com/blogspot/mstachniuk/solarsystem/task11/SolarSystemFactoryTest.java
+++ src/test/java/com/blogspot/mstachniuk/solarsystem/task11/SolarSystemFactoryTest.java
@@ -6,13 +6,9 @@ import com.blogspot.mstachniuk.solarsystem.RotationDirection;
 import com.blogspot.mstachniuk.solarsystem.task07.SolarSystemFactory;
 import org.junit.Test;
 
-import java.math.BigDecimal;
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
-import static org.junit.Assert.assertEquals;
+import static com.blogspot.mstachniuk.solarsystem.task11.PlanetSetAssert.assertThat;
 
 public class SolarSystemFactoryTest {
 
@@ -30,33 +26,47 @@ public class SolarSystemFactoryTest {
         List<Planet> innerPlanets = factory.createInnerPlanets();
 
         // then
-        Planet mercury = innerPlanets.get(0);
-        assertPlanet(mercury, "Mercury", RotationDirection.LEFT, "4879400", "87.96935", "47.362",
-                3.701, Gas.OXYGEN, Gas.SODIUM, Gas.HYDROGEN);
+        assertThat(innerPlanets)
+                .containsPlanetWithName3("Mercury")
+                .hasRotation(RotationDirection.LEFT)
+                .hasDiameterInMeter("4879400")
+                .hasYearInEarthDays("87.96935")
+                .hasAvgOrbitalSpeedInKmPerSecond("47.362")
+                .hasAcceleration(3.701)
+                .containsGas(Gas.OXYGEN)
+                .containsGas(Gas.SODIUM)
+                .containsGas(Gas.HYDROGEN);
 
-        Planet venus = innerPlanets.get(1);
-        assertPlanet(venus, "Venus", RotationDirection.RIGHT, "12103700", "224.70096", "35.02",
-                8.87, Gas.CARBON_DIOXIDE, Gas.NITROGEN);
+        assertThat(innerPlanets)
+                .containsPlanetWithName3("Venus")
+                .hasRotation(RotationDirection.RIGHT)
+                .hasDiameterInMeter("12103700")
+                .hasYearInEarthDays("224.70096")
+                .hasAvgOrbitalSpeedInKmPerSecond("35.02")
+                .hasAcceleration(8.87)
+                .containsOnlyGases(Gas.CARBON_DIOXIDE, Gas.NITROGEN);
 
-        Planet earth = innerPlanets.get(2);
-        assertPlanet(earth, "Earth", RotationDirection.LEFT, "12756273", "365.256363004", "29.78",
-                9.806_65, Gas.NITROGEN, Gas.OXYGEN, Gas.CARBON_DIOXIDE, Gas.ARGON);
+        assertThat(innerPlanets)
+                .containsPlanetWithName3("Earth")
+                .hasRotation(RotationDirection.LEFT)
+                .hasDiameterInMeter("12756273")
+                .hasYearInEarthDays("365.256363004")
+                .hasAvgOrbitalSpeedInKmPerSecond("29.78")
+                .hasAcceleration(9.806_65)
+                .containsGases(Gas.NITROGEN, Gas.OXYGEN)
+                .doesNotContainGases(Gas.SODIUM);
 
-        Planet mars = innerPlanets.get(3);
-        assertPlanet(mars, "Mars", RotationDirection.LEFT, "6804900", "686.9601", "24.077",
-                3.69, Gas.CARBON_DIOXIDE, Gas.NITROGEN);
-    }
+        assertThat(innerPlanets)
+                .containsPlanetWithName3("Mars")
+                .hasRotation(RotationDirection.LEFT)
+                .hasDiameterInMeter("6804900")
+                .hasYearInEarthDays("686.9601")
+                .hasAvgOrbitalSpeedInKmPerSecond("24.077")
+                .hasAcceleration(3.69)
+                .containsGases(Gas.CARBON_DIOXIDE, Gas.NITROGEN)
+                .doesNotContainGases(Gas.OXYGEN);
 
-    private void assertPlanet(Planet planet, String planetName, RotationDirection direction, String diameterInMeter,
-                              String yearInEarthDays, String avgOrbitalSpeedInKmPerSecond, double acceleration,
-                              Gas... atmosphereGases) {
-        assertEquals(planetName, planet.getName());
-        assertEquals(direction, planet.getRotationDirection());
-        assertEquals(0, new BigDecimal(diameterInMeter).compareTo(planet.getDiameter().getMeter()));
-        assertEquals(0, new BigDecimal(yearInEarthDays).compareTo(planet.getSiderealYear().inEarthDays()));
-        assertEquals(0, new BigDecimal(avgOrbitalSpeedInKmPerSecond).compareTo(planet.getAvgOrbitalSpeed().getKmPerSecond()));
-        assertEquals(acceleration, planet.getAcceleration(), 0.01);
-        Set<Gas> expectedGases = Stream.of(atmosphereGases).collect(Collectors.toSet());
-        assertEquals(expectedGases, planet.getAtmosphereGases());
+        assertThat(innerPlanets)
+                .containsOnlyPlanets("Mercury", "Venus", "Earth", "Mars");
     }
 }
