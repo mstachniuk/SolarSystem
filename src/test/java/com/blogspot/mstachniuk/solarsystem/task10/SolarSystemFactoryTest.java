e98b5f32190318dfe24747f1f50503a8d018ec4b
Marcin Stachniuk <mstachniuk@gmail.com>
2014-09-26 11:22:13 +0200

    solution task 10

--- src/test/java/com/blogspot/mstachniuk/solarsystem/task10/SolarSystemFactoryTest.java
+++ src/test/java/com/blogspot/mstachniuk/solarsystem/task10/SolarSystemFactoryTest.java
@@ -1,9 +1,12 @@
 package com.blogspot.mstachniuk.solarsystem.task10;
 
-import com.blogspot.mstachniuk.solarsystem.Planet;
+import com.blogspot.mstachniuk.solarsystem.*;
 import com.blogspot.mstachniuk.solarsystem.task07.SolarSystemFactory;
+import com.blogspot.mstachniuk.solarsystem.vo.Distance;
+import com.blogspot.mstachniuk.solarsystem.vo.Speed;
 import org.junit.Test;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 public class SolarSystemFactoryTest {
@@ -21,45 +24,45 @@ public class SolarSystemFactoryTest {
         List<Planet> innerPlanets = factory.createInnerPlanets();
 
         // then
-//        Planet mercury = innerPlanets.get(0);
-//        PlanetAssert.assertThat(mercury)
-//                .hasName("Mercury")
-//                .hasRotationDirection(RotationDirection.LEFT)
-//                .hasDiameter(Distance.createFromMeter(new BigDecimal("4879400")))
-//                .hasSiderealYear(new SiderealYear(new BigDecimal("87.96935")))
-//                .hasAvgOrbitalSpeed(Speed.createKmPerSecond("47.362"))
-//                .hasAcceleration(3.701)
-//                .hasAtmosphereGases(Gas.OXYGEN, Gas.SODIUM, Gas.HYDROGEN);
-//
-//        Planet venus = innerPlanets.get(1);
-//        PlanetAssert.assertThat(venus)
-//                .hasName("Venus")
-//                .hasRotationDirection(RotationDirection.RIGHT)
-//                .hasDiameter(Distance.createFromMeter(new BigDecimal("12103700")))
-//                .hasSiderealYear(new SiderealYear(new BigDecimal("224.70096")))
-//                .hasAvgOrbitalSpeed(Speed.createKmPerSecond("35.02"))
-//                .hasAcceleration(8.87)
-//                .hasAtmosphereGases(Gas.CARBON_DIOXIDE, Gas.NITROGEN);
-//
-//        Planet earth = innerPlanets.get(2);
-//        PlanetAssert.assertThat(earth)
-//                .hasName("Earth")
-//                .hasRotationDirection(RotationDirection.LEFT)
-//                .hasDiameter(Distance.createFromMeter(new BigDecimal("12756273")))
-//                .hasSiderealYear(new SiderealYear(new BigDecimal("365.256363004")))
-//                .hasAvgOrbitalSpeed(Speed.createKmPerSecond("29.78"))
-//                .hasAcceleration(9.806_65)
-//                .hasAtmosphereGases(Gas.NITROGEN, Gas.OXYGEN, Gas.CARBON_DIOXIDE, Gas.ARGON);
-//
-//        Planet mars = innerPlanets.get(3);
-//        PlanetAssert.assertThat(mars)
-//                .hasName("Mars")
-//                .hasRotationDirection(RotationDirection.LEFT)
-//                .hasDiameter(Distance.createFromMeter(new BigDecimal("6804900")))
-//                .hasSiderealYear(new SiderealYear(new BigDecimal("686.9601")))
-//                .hasAvgOrbitalSpeed(Speed.createKmPerSecond("24.077"))
-//                .hasAcceleration(3.69)
-//                .hasAtmosphereGases(Gas.CARBON_DIOXIDE, Gas.NITROGEN);
+        Planet mercury = innerPlanets.get(0);
+        PlanetAssert.assertThat(mercury)
+                .hasName("Mercury")
+                .hasRotationDirection(RotationDirection.LEFT)
+                .hasDiameter(Distance.createFromMeter(new BigDecimal("4879400")))
+                .hasSiderealYear(new SiderealYear(new BigDecimal("87.96935")))
+                .hasAvgOrbitalSpeed(Speed.createKmPerSecond("47.362"))
+                .hasAcceleration(3.701)
+                .hasAtmosphereGases(Gas.OXYGEN, Gas.SODIUM, Gas.HYDROGEN);
+
+        Planet venus = innerPlanets.get(1);
+        PlanetAssert.assertThat(venus)
+                .hasName("Venus")
+                .hasRotationDirection(RotationDirection.RIGHT)
+                .hasDiameter(Distance.createFromMeter(new BigDecimal("12103700")))
+                .hasSiderealYear(new SiderealYear(new BigDecimal("224.70096")))
+                .hasAvgOrbitalSpeed(Speed.createKmPerSecond("35.02"))
+                .hasAcceleration(8.87)
+                .hasAtmosphereGases(Gas.CARBON_DIOXIDE, Gas.NITROGEN);
+
+        Planet earth = innerPlanets.get(2);
+        PlanetAssert.assertThat(earth)
+                .hasName("Earth")
+                .hasRotationDirection(RotationDirection.LEFT)
+                .hasDiameter(Distance.createFromMeter(new BigDecimal("12756273")))
+                .hasSiderealYear(new SiderealYear(new BigDecimal("365.256363004")))
+                .hasAvgOrbitalSpeed(Speed.createKmPerSecond("29.78"))
+                .hasAcceleration(9.806_65)
+                .hasAtmosphereGases(Gas.NITROGEN, Gas.OXYGEN, Gas.CARBON_DIOXIDE, Gas.ARGON);
+
+        Planet mars = innerPlanets.get(3);
+        PlanetAssert.assertThat(mars)
+                .hasName("Mars")
+                .hasRotationDirection(RotationDirection.LEFT)
+                .hasDiameter(Distance.createFromMeter(new BigDecimal("6804900")))
+                .hasSiderealYear(new SiderealYear(new BigDecimal("686.9601")))
+                .hasAvgOrbitalSpeed(Speed.createKmPerSecond("24.077"))
+                .hasAcceleration(3.69)
+                .hasAtmosphereGases(Gas.CARBON_DIOXIDE, Gas.NITROGEN);
     }
 
 }
